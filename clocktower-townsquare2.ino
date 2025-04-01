c9717dbb7f9143bf693d4aa2fbe9f4c965b525e3
Joachim Jellinek <jake.jellinek@flowminder.org>
2025-04-01 11:33:57 +0100

    colour change

--- clocktower-townsquare2.ino
+++ clocktower-townsquare2.ino
@@ -358,6 +358,7 @@ String getHTMLStyles() {
         opacity: 0.7;
       }
       .status-btn {
+        background-color: #3a3a3a;
         color: white;
         font-weight: bold;
       }
@@ -592,10 +593,10 @@ String getHTMLJavaScript() {
 
 String generateHTMLPage() {
   String html = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'>";
-  html += "<title>Blood on the Clocktower</title>";
+  html += "<title>BOTC Townsquare</title>";
   html += getHTMLStyles();
   html += "</head><body>";
-  html += "<h1>Blood on the Clocktower</h1>";
+  html += "<h1>BOTC Townsquare</h1>";
   
   // Game Controls Section
   html += "<div class='controls'>";
