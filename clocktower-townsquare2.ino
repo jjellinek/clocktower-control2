39ac713b51d24222cf3d944217ea969695b56f85
Joachim Jellinek <jake.jellinek@flowminder.org>
2025-04-03 17:16:02 +0100

    Port change

--- clocktower-townsquare2.ino
+++ clocktower-townsquare2.ino
@@ -35,7 +35,7 @@
 Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
 
 // LEDs
-#define DATA_PIN 12  // GPIO8 for RGB light beads (WS2812)
+#define DATA_PIN 3  // GPIO8 for RGB light beads (WS2812)
 #define NUM_PLAYERS 20
 #define LEDS_PER_PLAYER 3
 #define NUM_LEDS (NUM_PLAYERS * LEDS_PER_PLAYER)
